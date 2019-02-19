3c76deaa743295dc73cae781465bf8b9747b4aee
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-02-19 15:05:25 -0500

    removes /build directory from .gitignore

---
+++ build/precache-manifest.e5c72f4dab65cbfacb9d0e1c52716d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "0b9199a502634757a9bf",
+    "url": "/ravenous/static/js/runtime~main.0b9199a5.js"
+  },
+  {
+    "revision": "315a6251b792b73692c5",
+    "url": "/ravenous/static/js/main.315a6251.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/ravenous/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "315a6251b792b73692c5",
+    "url": "/ravenous/static/css/main.4f2fda03.chunk.css"
+  },
+  {
+    "revision": "b067210e1202817d05fbbea3904c8f0b",
+    "url": "/ravenous/index.html"
+  }
+];
